352ce5e4fd4ce02091f94170e3499179d39c36e7
Joshua Rogan <c17488402@mydit.ie>
2019-01-31 20:26:12 +0000

    added mongolab

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes       = require("./routes/comments"),
 
 
 // seedDB(); // seed the database
-mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+mongoose.connect('mongodb://josh:Celtic11@ds245971.mlab.com:45971/imagesite', { useNewUrlParser: true });
+//mongodb://josh:Celtic11@ds245971.mlab.com:45971/imagesite
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
