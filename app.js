107768d2f2fbcb9ba1d613a3b80dd1d7bf6c2cd1
Joshua Rogan <c17488402@mydit.ie>
2019-01-31 20:47:50 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,11 +16,10 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index")
 
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+//mongoose.connect('mongodb://josh:Celtic11@ds245971.mlab.com:45971/imagesite', { useNewUrlParser: true });
 
-// seedDB(); // seed the database
-//mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
-mongoose.connect('mongodb://josh:Celtic11@ds245971.mlab.com:45971/imagesite', { useNewUrlParser: true });
-//mongodb://josh:Celtic11@ds245971.mlab.com:45971/imagesite
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
